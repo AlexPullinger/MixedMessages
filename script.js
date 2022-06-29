cc36930e14b31ca87ed752714e173c9116a908cf
Alex Pullinger <alex.pullinger@gmail.com>
2022-06-29 21:00:23 +0100

    Add joke function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+// Joke opening lines go in this array
 const openerArray = [];
 
+// Joke punchlines go in this array
 const punchlineArray = [];
 
+// Generate random opening line and random punchline
+const generateRandomJoke = (opener, punchline) => {
+    let openerRandomNumber = Math.floor(Math.random() * opener.length);
+    let punchlineRandomNumber = Math.floor(Math.random() * punchline.length);
+    console.log(opener[openerRandomNumber]);
+    console.log(punchline[punchlineRandomNumber]);
+}
+
+generateRandomJoke(openerArray, punchlineArray);
