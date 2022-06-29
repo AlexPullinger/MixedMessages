02f2b76b98ffcc1aaade5b54ad3135744c6b2739
Alex Pullinger <alex.pullinger@gmail.com>
2022-06-29 21:07:32 +0100

    fill arrays

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Joke opening lines go in this array
-const openerArray = [];
+const openerArray = ["My wife told me to stop impersonating a flamingo.","I was wondering why the frisbee kept getting bigger and bigger,","I heard there were a bunch of break-ins over at the car park.","I want to die peacefully in my sleep, like my grandfather.","When life gives you melons,","Don’t you hate it when someone answers their own questions?","My wife just found out I replaced our bed with a trampoline."];
 
 // Joke punchlines go in this array
-const punchlineArray = [];
+const punchlineArray = ["I had to put my foot down.", "but then it hit me.","That is wrong on so many levels.","Not screaming and yelling like the passengers in his car.","you might be dyslexic.","I do.","She hit the roof!"];
 
 // Generate random opening line and random punchline
 const generateRandomJoke = (opener, punchline) => {
